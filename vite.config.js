2c4057ea7502c8fefa133129db3d07a99317d326
DevendraVarma <ravi7657dev@gmail.com>
2025-02-20 12:20:55 +0530

    Configured for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Change this if needed (see note below)
+  base: "/Interactive-Quiz/", // GitHub repo name
 });
